b426f68b464953487e3b6234e61881e53a4813dc
MadZander1 <69798725+MadZander1@users.noreply.github.com>
2020-09-30 08:50:50 +0200

    first commit

---
+++ R/test.R
@@ -0,0 +1,8 @@
+test_function <- function(test=TRUE){
+  if(test==TRUE){
+    print("I love tests!")
+  }
+  else {
+    print("test did not work.")
+  }
+}
---
+++ R/test_function.R
@@ -0,0 +1,17 @@
+#' A test Function
+#'
+#' This function allows you to test.
+#' @param test Defaults to TRUE.
+#' @keywords test
+#' @export
+#' @examples
+#' test_function()
+#'
+test_function <- function(test=TRUE){
+  if(test==TRUE){
+    print("I love THUENEN tests!")
+  }
+  else {
+    print("THUENEN test did not work.")
+  }
+}
